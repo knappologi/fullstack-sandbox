f4e48efe1070e45cbf0af7bddf3e0a25bbc70676
kanppologi <nejfredrika@gmail.com>
2019-11-11 00:17:25 +0100

    cleaner

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,65 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = 3001
+const PORT = 3001;
 
 let toDoLists = {
-    "0000000001": {
-        id: "0000000001",
-        title: "First List",
-        todos: ["First todo of first list!"]
-    },
-    "0000000002": {
-        id: "0000000002",
-        title: "Second List",
-        todos: ["First todo of second list!"]
-    },
-    "0000000003": {
-        id: "0000000003",
-        title: "Bonus List",
-        todos: ["First todo of bonus list!"]
-    }
-}
+  '0000000001': {
+    id: '0000000001',
+    title: 'First List',
+    done: false,
+    todos: [{ text: 'First todo of first list!', done: false }]
+  },
+  '0000000002': {
+    id: '0000000002',
+    title: 'Second List',
+    done: false,
+    todos: [{ text: 'First todo of second list!', done: false }]
+  },
+  '0000000003': {
+    id: '0000000003',
+    title: 'Bonus List',
+    done: false,
+    todos: [{ text: 'First todo of bonus list!', done: false }]
+  }
+};
 
-app.get('/', (req, res) => res.send('Hello cuties!'))
+app.get('/', (req, res) => res.send('Hello cuties!'));
 
-app.get('/lists', (req, res) => res.send(toDoLists))
+app.get('/lists', (req, res) => res.send(toDoLists));
 
 /**
  * Post to all lists
  */
 app.post('/lists', (req, res) => {
-    toDoLists[req.body.id].todos = req.body.todos;
-    res.send('Got lists!');
+  toDoLists[req.body.id].todos = req.body.todos;
+  res.send('Got the new lists, thank you!');
 });
 
 /**
  * Update to all lists
  */
 app.put('/lists/update', (req, res) => {
-    toDoLists[req.body.id].todos = req.body.todos;
-    res.send('Got lists!');
+  const listId = req.body.id;
+  checkIfListComplete(req.body.todos, toDoLists[listId]);
+  toDoLists[listId].todos = req.body.todos;
+  res.send('Got the lists and will update, thank you!');
 });
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+const checkIfListComplete = (todoList, list) => {
+  list.done = true;
+  todoList.forEach(todoItem => {
+    if (!todoItem.done) {
+      list.done = false;
+    }
+  });
+};
+
+app.listen(PORT, () =>
+  console.log(`Hello cuties! Todo-list now on port ${PORT}!`)
+);
